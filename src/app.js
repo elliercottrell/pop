b98cc6fd2f1d5eaf741007ae3cd9e7cd2a8e9ede
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-06-28 21:44:09 +0100

    updated words

--- src/app.js
+++ src/app.js
@@ -7,3 +7,23 @@ function myFunctionTwo() {
   var popupTwo = document.getElementById("myPopupTwo");
   popupTwo.classList.toggle("showTwo");
 }
+
+function myFunctionThree() {
+  var popupThree = document.getElementById("myPopupThree");
+  popupThree.classList.toggle("showThree");
+}
+
+function myFunctionFour() {
+  var popupFour = document.getElementById("myPopupFour");
+  popupFour.classList.toggle("showFour");
+}
+
+function myFunctionFive() {
+  var popupFive = document.getElementById("myPopupFive");
+  popupFive.classList.toggle("showFive");
+}
+
+function myFunctionSix() {
+  var popupSix = document.getElementById("myPopupSix");
+  popupSix.classList.toggle("showSix");
+}
