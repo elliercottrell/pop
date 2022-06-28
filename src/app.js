fca2f9a6d0da27c1545f0fdfeb5ca2a7d77b81e0
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2022-06-28 21:16:05 +0100

    Added index.html, style.css, app.js

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function myFunctionTwo() {
+  var popupTwo = document.getElementById("myPopupTwo");
+  popupTwo.classList.toggle("showTwo");
+}
